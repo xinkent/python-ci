afac285b1d4d443ea3a8b25f15aa3cb9e42414fc
xinkent <xinkent1994@gmail.com>
2020-06-15 18:56:34 +0900

    fix testcode

--- tests/math-test.py
+++ tests/math-test.py
@@ -1,7 +1,8 @@
 import unittest
-from src.math import Math
+from src.math import addition
 
 class MathTest(unittest.TestCase):
     def test_addition(self):
-        # Make test fail
-        self.assertEqual(Math.addition(3, 4), 8)
+        actual = addition(3,4)
+        expected = 8
+        self.assertEqual(actual, expected)
